26be715947fe68e8d2fd9be8f34d5f3927396e78
Will Huang <will.xing@outlook.com>
2020-08-14 00:04:06 +1200

    Some ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,39 @@
-import PieChart from "../components/PieChart";
 import ConfirmedCaseChart from "../components/ConfirmedCaseChart";
 import ProbableCaseChart from "../components/ProbableCaseChart";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { useState } from "react";
+
 export default () => {
+  const [tab, setTab] = useState(0);
   return (
     <div>
-      <h1>VISUAL COVID NZ</h1>
-      <div style={{ height: "600px" }}>
-        <ConfirmedCaseChart />
-      </div>
-      <div style={{ height: "600px" }}>
-        <ProbableCaseChart />
-      </div>
+      <Typography variant="h2" component="h2">
+        VISUAL COVID NZ
+      </Typography>
+      <AppBar position="static">
+        <Tabs
+          value={tab}
+          onChange={(e, value) => setTab(value)}
+          aria-label="simple tabs example"
+        >
+          <Tab label="Confirmed Cases" />
+          <Tab label="Probable Cases" />
+        </Tabs>
+      </AppBar>
+      <Paper hidden={tab != 0}>
+        <div style={{ height: "600px" }}>
+          <ConfirmedCaseChart />
+        </div>
+      </Paper>
+      <Paper hidden={tab != 1}>
+        <div style={{ height: "600px" }}>
+          <ProbableCaseChart />
+        </div>
+      </Paper>
     </div>
   );
 };
